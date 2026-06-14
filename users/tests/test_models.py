8370033d2f02316f53c0205409ad1313f4720340
Vladislav Lukin <vladislavlukin090392@gmail.com>
2026-06-14 06:50:55 +0400

    added tests for users

---
+++ users/tests/test_models.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+from users.models import CustomUser
+
+class CustomUserModelTestCase(TestCase):
+
+    def test_str_returns_email(self):
+        user = CustomUser.objects.create(
+            email="test@example.com",
+            phone_number="1234567890",
+            first_name="Test",
+    )
+
+        self.assertEqual(str(user), "test@example.com")
+
